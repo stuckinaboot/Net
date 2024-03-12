fa20f6903b863cc1cd7927e9a393ffeaac90166d
stuckinaboot <paluter@gmail.com>
2024-03-12 17:06:24 -0400

    update

--- protocol/src/warhol/SharedConstants.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity >=0.8.17 .0;
-
-contract SharedConstants {
-    uint8 internal constant SMALL_TOKEN_COUNT = 4;
-    uint8 internal constant LARGE_TOKEN_COUNT = 9;
-}
