da61c1861927a9021ef2884321a3278dcefc76d6
stuckinaboot <paluter@gmail.com>
2024-03-12 21:44:50 -0400

    update

--- website/willienet/src/app/SendMessageButton.tsx
+++ website/willienet/src/app/SendMessageButton.tsx
@@ -1,26 +1,34 @@
-import { useWaitForTransactionReceipt, useWriteContract } from 'wagmi'
-import { WILLIE_NET_CONTRACT } from './constants'
+import { useWaitForTransactionReceipt, useWriteContract } from "wagmi";
+import { WILLIE_NET_CONTRACT } from "./constants";
 
-export default function SendMessageButton(props: { tokenId: number, message: string, topic: string }) {
-    const { data: hash, writeContractAsync, status } = useWriteContract()
-    const receipt = useWaitForTransactionReceipt({ hash })
+export default function SendMessageButton(props: {
+  tokenId: number;
+  message: string;
+  topic: string;
+}) {
+  const { data: hash, writeContractAsync, status } = useWriteContract();
+  const receipt = useWaitForTransactionReceipt({ hash });
 
+  async function performTransaction() {
+    await writeContractAsync({
+      address: WILLIE_NET_CONTRACT.address as any,
+      abi: WILLIE_NET_CONTRACT.abi,
+      functionName: "sendMessage",
+      args: [
+        BigInt(props.tokenId),
+        "0x0000000000000000000000000000000000000000000000000000000000000000",
+        props.message,
+        props.topic,
+      ],
+    });
+  }
 
-    async function performTransaction() {
-        await writeContractAsync({
-            address: WILLIE_NET_CONTRACT.address as any,
-            abi: WILLIE_NET_CONTRACT.abi,
-            functionName: 'sendMessage',
-            args: [BigInt(props.tokenId), '0x0000000000000000000000000000000000000000000000000000000000000000', props.message, props.topic],
-        })
-    }
-
-    return <>
-        <button onClick={performTransaction}>Send message</button>
-        <textarea value={`message: ${props.message}`} readOnly />
-        <textarea value={`tx submission: ${status}`} readOnly />
-        <textarea value={`tx receipt: ${receipt.status} ${hash}`} readOnly />
+  return (
+    <>
+      <button onClick={performTransaction}>Send message</button>
+      <textarea value={`message: ${props.message}`} readOnly />
+      <textarea value={`tx submission: ${status}`} readOnly />
+      <textarea value={`tx receipt: ${receipt.status} ${hash}`} readOnly />
     </>
+  );
 }
-
-
