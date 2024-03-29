bb382ffa28d951fcbd43d4dcd892f4f792640919
stuckinaboot <paluter@gmail.com>
2024-03-28 23:01:43 -0600

    update

--- website/willienet/src/app/providers.tsx
+++ website/willienet/src/app/providers.tsx
@@ -6,6 +6,11 @@ import {
   getDefaultWallets,
   getDefaultConfig,
 } from "@rainbow-me/rainbowkit";
+import {
+  argentWallet,
+  trustWallet,
+  ledgerWallet,
+} from "@rainbow-me/rainbowkit/wallets";
 import { base, baseSepolia } from "wagmi/chains";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { WagmiProvider } from "wagmi";
@@ -17,7 +22,13 @@ const config = getDefaultConfig({
   appName: "WillieNet",
   // TODO move to env var
   projectId: "e30601719b43774a0f0ba554aa131083",
-  wallets,
+  wallets: [
+    ...wallets,
+    {
+      groupName: "Other",
+      wallets: [argentWallet, trustWallet, ledgerWallet],
+    },
+  ],
   chains: testnetsEnabled ? [baseSepolia] : [base],
   ssr: true,
 });
