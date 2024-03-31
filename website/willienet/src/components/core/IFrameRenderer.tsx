f4ce1f9928acb6d6de50c822e000537f856384fd
stuckinaboot <paluter@gmail.com>
2024-03-30 22:08:56 -0600

    update

--- website/willienet/src/components/core/IFrameRenderer.tsx
+++ website/willienet/src/components/core/IFrameRenderer.tsx
@@ -4,7 +4,7 @@ export default function IframeRenderer({ htmlString }: { htmlString: string }) {
       className="bg-white"
       title="Rendered HTML"
       srcDoc={htmlString}
-      style={{ width: "100%", height: "100%", border: "none" }}
+      style={{ width: "100%", height: "400px", border: "none" }}
     />
   );
 }
