b4a7609d84c20be951a2499d40c4f998d143530c
stuckinaboot <paluter@gmail.com>
2024-04-03 19:53:24 -0600

    update

--- website/willienet/src/components/core/SendMessageSection.tsx
+++ website/willienet/src/components/core/SendMessageSection.tsx
@@ -3,14 +3,15 @@ import { cn } from "@/lib/utils";
 import React, { useState } from "react";
 import SendMessageButton, { Nft } from "./SendMessageButton";
 import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
 
 export default function SendMessageSection(props: { nft?: Nft }) {
   const [message, setMessage] = useState("");
 
   return (
     <div className="flex flex-col items-center justify-between w-full">
-      <Input
-        placeholder="Enter message to send"
+      <Textarea
+        placeholder="Enter message to send..."
         contentEditable
         onChange={(e) => {
           const txt = e.target.value;
@@ -18,6 +19,15 @@ export default function SendMessageSection(props: { nft?: Nft }) {
         }}
         value={message}
       />
+      {/* <Input
+        placeholder="Enter message to send"
+        contentEditable
+        onChange={(e) => {
+          const txt = e.target.value;
+          setMessage(txt);
+        }}
+        value={message}
+      /> */}
       <div className="m-1" />
       <SendMessageButton
         nft={props.nft}
