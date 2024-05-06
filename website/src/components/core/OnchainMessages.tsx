0e6a3db168572ce015d901a461a5d6fcb6d13794
stuckinaboot <paluter@gmail.com>
2024-05-06 00:13:46 -0400

    update

--- website/src/components/core/OnchainMessages.tsx
+++ website/src/components/core/OnchainMessages.tsx
@@ -23,7 +23,7 @@ const GLOBAL_CHAT_ROOM_ITEM = "Global";
 const ONCHAIN_STEAMBOAT_WILLIES_CHAT_ROOM_ITEM = "Onchain Steamboat Willies";
 const CHAT_ROOM_ITEMS = [
   GLOBAL_CHAT_ROOM_ITEM,
-  ONCHAIN_STEAMBOAT_WILLIES_CHAT_ROOM_ITEM,
+  // ONCHAIN_STEAMBOAT_WILLIES_CHAT_ROOM_ITEM,
 ];
 
 function nftAddressFromChatRoomItem(chatRoomItem: string) {
