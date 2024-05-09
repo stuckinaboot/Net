e6309447ed1f73b883e6b5dff2745b7fea322db6
stuckinaboot <paluter@gmail.com>
2024-05-08 23:47:13 -0400

    update nft gated address to be dynamic in app config

--- website/src/components/core/net-apps/AppManager.tsx
+++ website/src/components/core/net-apps/AppManager.tsx
@@ -1,6 +1,7 @@
 import { AppComponentsConfig } from "../types";
 import { config as NftGatingAppConfig } from "./nft-gating/NftGatingAppConfig";
+import { NFT_GATED_CHAT_CONTRACT } from "./nft-gating/constants";
 
 export const APP_TO_CONFIG: { [address: string]: AppComponentsConfig } = {
-  "0x3d9d7f4646028847c84539855e2d8d63ebe11991": NftGatingAppConfig,
+  [NFT_GATED_CHAT_CONTRACT.address]: NftGatingAppConfig,
 };
