656a9de45c4ff2775c2cd9f2b093aacd6d5463f4
stuckinaboot <paluter@gmail.com>
2024-05-17 00:28:00 -0400

    update

--- protocol/test/apps/inscribed-drops/InscribedDrops.t.sol
+++ protocol/test/apps/inscribed-drops/InscribedDrops.t.sol
@@ -8,13 +8,9 @@ import {Net} from "../../../src/net/Net.sol";
 import {IERC721Receiver} from "@openzeppelin/contracts/interfaces/IERC721Receiver.sol";
 import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
 import {InscribedDrops} from "../../../src/apps/inscribed-drops/InscribedDrops.sol";
+import {PRBTest} from "@prb/test/PRBTest.sol";
 
-contract InscribedDropsTest is
-    EventsAndErrors,
-    TestUtils,
-    StdCheats,
-    IERC721Receiver
-{
+contract InscribedDropsTest is PRBTest, StdCheats {
     using stdStorage for StdStorage;
 
     StdStorage private stdstore;
@@ -23,6 +19,7 @@ contract InscribedDropsTest is
     address[10] users;
 
     Net public net = new Net();
+    InscribedDrops drops = new InscribedDrops();
 
     function setUp() public {
         vm.deal(address(this), 1000 ether);
@@ -31,6 +28,20 @@ contract InscribedDropsTest is
             users[i] = address(uint160(i + 1));
             vm.deal(users[i], 10 ether);
         }
+    }
+
+    function testInscribe() public {
+        string memory tokenUri = "abc";
+        uint256 mintPrice = 1;
+        uint256 maxSupply = 1;
+        uint256 mintEndTimestamp = 2;
+        drops.inscribe(mintPrice, maxSupply, mintEndTimestamp, "abc");
+
+        // Check total drops
+        assertEq(drops.totalDrops(), 1);
+
+        // Check total supply
+        assertEq(drops.totalSupply(0), 1);
     }
 
     receive() external payable {}
