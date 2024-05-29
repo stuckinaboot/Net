58d36bcdba6b666d53820d1ac37a6dd009057386
stuckinaboot <paluter@gmail.com>
2024-05-29 12:30:01 -0400

    update

--- website/src/components/core/net-apps/inscribed-drops/constants.ts
+++ website/src/components/core/net-apps/inscribed-drops/constants.ts
@@ -7,7 +7,7 @@ export const INSCRIBED_DROPS_COLLECTION_URL = TESTNETS_ENABLED
 
 export const INSCRIBED_DROPS_CONTRACT = {
   abi,
-  address: "0x00005c7a0aa9d319ea48d25d89159c088270be9a",
+  address: "0x00000000c7AAcaEb85756F9bc59B0D0D6eee95cf",
 };
 
 // Corresponds to the constant in the Inscribed Drops contract
