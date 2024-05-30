cc1ad068ec67a01b85044d61e1e3c516b55d65d8
stuckinaboot <paluter@gmail.com>
2024-05-30 00:38:47 -0400

    update

--- website/src/components/core/DefaultMessageRenderer.tsx
+++ website/src/components/core/DefaultMessageRenderer.tsx
@@ -1,5 +1,6 @@
 import {
   chainIdToChain,
+  chainIdToOpenSeaChainString,
   chainTimeToMilliseconds,
   publicClient,
 } from "@/app/utils";
@@ -10,7 +11,9 @@ import CopyMessageLinkButton from "./CopyMessageLinkButton";
 import { readContract } from "viem/actions";
 import memoize from "memoizee";
 import useAsyncEffect from "use-async-effect";
-import { useState } from "react";
+import React, { useState } from "react";
+import { INSCRIBED_DROPS_CONTRACT } from "./net-apps/inscribed-drops/constants";
+import Link from "next/link";
 
 const SHOW_MESSAGE_TIMESTAMP = false;
 
@@ -42,22 +45,88 @@ const getAppName = memoize(async (appAddress: string, chainId: number) => {
   }
 });
 
+const transformedMessageForApp = memoize(
+  async (
+    appAddress: string,
+    chainId: number,
+    message: string
+  ): Promise<string | React.ReactNode> => {
+    try {
+      const hashTagIndex = message.indexOf("#");
+      if (hashTagIndex === -1) {
+        return message;
+      }
+      const dropId = message.substring(hashTagIndex + 1);
+      if (appAddress === INSCRIBED_DROPS_CONTRACT.address) {
+        const chain = chainIdToChain(chainId);
+        if (chain == null) {
+          return undefined;
+        }
+        const uri = (await readContract(publicClient(chain), {
+          address: appAddress as any,
+          abi: INSCRIBED_DROPS_CONTRACT.abi,
+          functionName: "uri",
+          args: [dropId],
+        })) as any;
+        const json = atob(
+          uri.substring("data:application/json;base64,".length)
+        );
+        const drop = JSON.parse(json);
+        if (drop.name) {
+          return (
+            <>
+              {message.substring(0, hashTagIndex)}
+              <Link
+                href={`/app/inscribed-drops/mint/${chainIdToOpenSeaChainString(
+                  chainId
+                )}/${dropId}`}
+              >
+                <p className="underline">{drop.name}</p>
+              </Link>
+            </>
+          );
+        }
+      } else {
+        return message;
+      }
+    } catch (e) {
+      // This may throw due to RPC errors or the contract not implementing the above function.
+      // In either case, gracefully return undefined
+      return undefined;
+    }
+  }
+);
+
 export default function DefaultMessageRenderer(props: AppMessageRendererProps) {
   const { idx, message } = props;
   const [appName, setAppName] = useState<string>();
+  const [finalMessage, setFinalMessage] = useState<React.ReactNode>(
+    props.message.text
+  );
 
   useAsyncEffect(async () => {
     const name = await getAppName(props.message.app, props.chainId);
     if (name != null) {
       setAppName(name);
     }
-  });
+  }, [props.message.app, props.chainId]);
+
+  useAsyncEffect(async () => {
+    const messageText = await transformedMessageForApp(
+      props.message.app,
+      props.chainId,
+      message.text
+    );
+    if (messageText != null) {
+      setFinalMessage(messageText);
+    }
+  }, [props.message.app, props.chainId, message.text]);
 
   return (
     <>
       <p className="flex text-left">
         {appName && `[${appName}] `}
-        {message.text}
+        {finalMessage}
       </p>
       <p className="flex justify-end">
         {SHOW_MESSAGE_TIMESTAMP && (
