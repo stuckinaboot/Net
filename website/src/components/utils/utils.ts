5d3a4b0143bedbca552b8e14e89eeff6c13f6e7e
stuckinaboot <paluter@gmail.com>
2024-05-30 14:53:01 -0400

    add inline

--- website/src/components/utils/utils.ts
+++ website/src/components/utils/utils.ts
@@ -1,23 +1,21 @@
 import { publicClient } from "@/app/utils";
 import { mainnet } from "viem/chains";
+import memoize from "memoizee";
 
-export async function getEnsName({
-  address,
-}: {
-  address: string;
-  chainId: number;
-}) {
-  const client = publicClient(mainnet);
-  // Use public client so backend isn't responsible for making these requests
-  try {
-    const ensName = await client.getEnsName({
-      address: address as any,
-    });
-    return ensName;
-  } catch (e) {
-    return null;
+export const getEnsName = memoize(
+  async ({ address }: { address: string; chainId: number }) => {
+    const client = publicClient(mainnet);
+    // Use public client so backend isn't responsible for making these requests
+    try {
+      const ensName = await client.getEnsName({
+        address: address as any,
+      });
+      return ensName;
+    } catch (e) {
+      return null;
+    }
   }
-}
+);
 
 export function getBlockTimestampAsLocalDateString(blockTimestamp: number) {
   return new Date(blockTimestamp * 1000).toLocaleString();
