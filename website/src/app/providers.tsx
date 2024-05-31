b05793c270819d62255ffd5e9034aa2ef8aad92f
stuckinaboot <paluter@gmail.com>
2024-05-31 01:25:59 -0400

    update

--- website/src/app/providers.tsx
+++ website/src/app/providers.tsx
@@ -11,7 +11,14 @@ import {
   trustWallet,
   ledgerWallet,
 } from "@rainbow-me/rainbowkit/wallets";
-import { base, baseSepolia, degen, sepolia } from "wagmi/chains";
+import {
+  base,
+  baseSepolia,
+  degen,
+  sepolia,
+  zora,
+  zoraSepolia,
+} from "wagmi/chains";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { WagmiProvider } from "wagmi";
 import { TESTNETS_ENABLED } from "./constants";
@@ -19,7 +26,7 @@ import { TESTNETS_ENABLED } from "./constants";
 const { wallets } = getDefaultWallets();
 
 const config = getDefaultConfig({
-  appName: "WillieNet Dapp",
+  appName: "Net Dapp",
   projectId: process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID as string,
   wallets: [
     ...wallets,
@@ -28,7 +35,9 @@ const config = getDefaultConfig({
       wallets: [argentWallet, trustWallet, ledgerWallet],
     },
   ],
-  chains: TESTNETS_ENABLED ? [baseSepolia, sepolia] : [base, degen],
+  chains: TESTNETS_ENABLED
+    ? [baseSepolia, zoraSepolia, sepolia]
+    : [base, degen, zora],
   ssr: true,
 });
 
--- website/src/components/core/SendMessageSection.tsx
+++ website/src/components/core/SendMessageSection.tsx
@@ -1,7 +1,33 @@
+import {
+  getClient,
+  Execute,
+  createClient,
+  MAINNET_RELAY_API,
+  convertViemChainToRelayChain,
+  TESTNET_RELAY_API,
+} from "@reservoir0x/relay-sdk";
 import React, { useState } from "react";
 import SendMessageButton from "./SendMessageButton";
 import { Textarea } from "@/components/ui/textarea";
 import { NetAppContext } from "./types";
+import { Button } from "../ui/button";
+import { createWalletClient, http } from "viem";
+import { useAccount, useWalletClient } from "wagmi";
+import { base, baseSepolia, mainnet, zora, zoraSepolia } from "viem/chains";
+import { WILLIE_NET_CONTRACT } from "@/app/constants";
+import { encodeAbiParameters } from "viem";
+import { encodeFunctionData } from "viem";
+
+const relayClient = createClient({
+  baseApiUrl: MAINNET_RELAY_API,
+  source: "relay-demo",
+  chains: [
+    convertViemChainToRelayChain(base),
+    convertViemChainToRelayChain(zora),
+    convertViemChainToRelayChain(baseSepolia),
+    convertViemChainToRelayChain(zoraSepolia),
+  ],
+});
 
 function getAppSendMessageDialog() {
   // TODO
@@ -12,6 +38,8 @@ export default function SendMessageSection(props: {
   disabled?: boolean;
 }) {
   const [message, setMessage] = useState("");
+  const account = useAccount();
+  const { data: wallet } = useWalletClient();
 
   return (
     <div className="flex flex-col items-center justify-between w-full">
@@ -26,6 +54,51 @@ export default function SendMessageSection(props: {
         disabled={props.disabled}
       />
       <div className="m-1" />
+      <Button
+        onClick={() => {
+          if (wallet == null) {
+            return;
+          }
+          const calldata = encodeFunctionData({
+            abi: WILLIE_NET_CONTRACT.abi,
+            functionName: "sendMessage",
+            args: ["Relay link what up", "", ""],
+          });
+          relayClient.actions.swap({
+            chainId: base.id, //This is not required as the call action will use the active configured chain
+            wallet: wallet,
+            txs: [
+              {
+                to: WILLIE_NET_CONTRACT.address,
+                data: calldata,
+                value: "0",
+              },
+            ],
+            toChainId: base.id,
+            currency: "0x0000000000000000000000000000000000000000",
+            amount: "10000000000000", // Amount in wei to swap
+            toCurrency: "0x4200000000000000000000000000000000000006",
+            onProgress: ({
+              steps,
+              currentStep,
+              currentStepItem,
+              txHashes,
+              details,
+            }) => {
+              console.log(
+                steps,
+                currentStep,
+                currentStepItem,
+                details,
+                txHashes
+              );
+            },
+            // options: { tradeType: "EXACT_OUTPUT" },
+          });
+        }}
+      >
+        foobar
+      </Button>
       <SendMessageButton
         appContext={props.appContext}
         className="w-full"
