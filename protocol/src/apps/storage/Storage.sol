42824063b0b78b9b75f8e680f6033443479a2d47
stuckinaboot <paluter@gmail.com>
2024-07-07 16:25:56 -0400

    update

---
+++ protocol/src/apps/storage/Storage.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.17 .0;
+
+import {Net} from "../../net/Net.sol";
+
+/// @title Storage
+/// @author Aspyn Palatnick (aspyn.eth, stuckinaboot.eth)
+/// @notice Data storage with persistent history using Net
+contract Storage {
+    Net internal net = Net(0x00000000B24D62781dB359b07880a105cD0b64e6);
+
+    function put(bytes32 key, bytes calldata value) external {
+        string memory topic = string(abi.encodePacked(key));
+        net.sendMessageViaApp(
+            msg.sender,
+            string.concat("Stored ", topic),
+            topic,
+            value
+        );
+    }
+
+    /// @notice Get data for a particular key and operator
+    /// @param key key
+    /// @param operator user that stored key
+    /// @return data stored data for the particular key-operator pair
+    function get(
+        bytes32 key,
+        address operator
+    ) external view returns (bytes memory) {
+        string memory topic = string(abi.encodePacked(key));
+        uint256 idx = net.getTotalMessagesForAppUserTopicCount(
+            address(this),
+            operator,
+            topic
+        );
+        Net.Message memory message = net.getMessageForAppUserTopic(
+            idx,
+            address(this),
+            operator,
+            topic
+        );
+        return message.data;
+    }
+}
